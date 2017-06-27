c0e7cfbb7be057431f3029515c794e52bc666748
Vitaly Rtishchev <rtivital@gmail.com>
2017-06-27 22:24:57 +0300

    add ui Button component

--- src/components/routes/home/HomePage.jsx
+++ src/components/routes/home/HomePage.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Button from 'components/ui/Button/Button';
 
 export default function HomePage() {
   return (
-    <div style={{ fontSize: 50, fontWeigth: 'bold', textAlign: 'center' }}>
-      React Pages Boilerplate
+    <div>
+      <h1 style={{ fontSize: 50, fontWeigth: 'bold', textAlign: 'center' }}>
+        React Pages Boilerplate
+      </h1>
+
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <Button theme="blue">Click me!</Button>
+      </div>
     </div>
   );
 }
