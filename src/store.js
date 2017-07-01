4b8367486c2bca2d2d5f3bed91f1a2ee7b8359b3
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-01 21:06:34 +0300

    remove localStorage store autosaving

--- src/store.js
+++ src/store.js
@@ -2,7 +2,6 @@ import { hashHistory } from 'react-router';
 import { routerMiddleware } from 'react-router-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import { throttle } from 'lodash';
 import rootReducer from 'modules';
 
 const routingMiddleware = routerMiddleware(hashHistory);
@@ -12,30 +11,7 @@ const middlewares = [
   routingMiddleware,
 ];
 
-function loadState() {
-  try {
-    const serializedState = JSON.parse(localStorage.getItem('state'));
-    if (serializedState === null) { return undefined; }
-    return serializedState;
-  } catch (e) {
-    return undefined;
-  }
-}
-
-function saveState(state) {
-  try {
-    // remove routing before saving: it ruins everything!
-    const stateToSave = { ...state };
-    delete stateToSave.routing;
-
-    localStorage.setItem('state', JSON.stringify(stateToSave));
-    return null;
-  } catch (e) {
-    return null;
-  }
-}
-
-const configureStore = (initialState = loadState()) => {
+export default function configureStore(initialState) {
   // Prevent redux devTools initialization in production
   const store = createStore(rootReducer, initialState, compose(
     applyMiddleware(...middlewares),
@@ -43,11 +19,6 @@ const configureStore = (initialState = loadState()) => {
       ? window.devToolsExtension()
       : f => f
   ));
-
-  // Save state to localStorage every second
-  store.subscribe(throttle(() => {
-    saveState(store.getState());
-  }, 1000));
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
@@ -59,6 +30,4 @@ const configureStore = (initialState = loadState()) => {
   }
 
   return store;
-};
-
-export default configureStore;
+}
