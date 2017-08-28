6e3b2290a96cbb3bc925a2aaf579c1e6107f0520
Vitaly Rtishchev <rtivital@gmail.com>
2017-08-28 17:41:02 +0300

    add repo capturing from cli in init script

--- scripts/init.js
+++ scripts/init.js
@@ -1,8 +1,20 @@
 /* eslint-disable no-console */
 
-const execute = require('child_process').exec;
+const execute = require('child-process-promise').exec;
 const chalk = require('chalk');
+const argv = require('yargs').argv;
 
-console.log(chalk.green.bold`
-  Initing react-pages-boilerplate
-`);
+// console.log(chalk.green.bold`Initing react-pages-boilerplate`);
+
+if (!argv.repo) {
+  console.log(`
+    ${chalk.red.bold`Initing react-pages-boilerplate failed: `} Missign repository link
+    You should run init command with repo argumnet ${chalk.cyan.bold`npm run init -- --repo https://github.com/username/reponame`}
+  `);
+}
+
+execute('echo "Hello world"')
+  .catch((err) => {
+    console.log(`Error occurred while running init script: ${chalk.red.bold(err)}\n`);
+    process.exit(1);
+  });
