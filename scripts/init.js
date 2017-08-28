6fad576d4e85cbd62b3f2f8a23399cfb293a1316
Vitaly Rtishchev <rtivital@gmail.com>
2017-08-28 17:11:07 +0300

    add new init script

---
+++ scripts/init.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+
+const execute = require('child_process').exec;
+const chalk = require('chalk');
+
+console.log(chalk.green.bold`
+  Initing react-pages-boilerplate
+`);
