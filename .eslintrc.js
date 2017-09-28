d7fa40f90e4563744e1aafb23ed84ac2591d40d2
Vitaly Rtishchev <rtivital@gmail.com>
2017-09-28 13:15:16 +0300

    clean eslint conffiguration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,28 @@
 module.exports = {
-  "parser": "babel-eslint",
-  "extends": ["airbnb"],
-  "env": {
-    "browser": true,
-    "node": true
+  parser: 'babel-eslint',
+  extends: ['airbnb'],
+  env: {
+    browser: true,
+    node: true
   },
-  "rules": {
-    "react/no-danger": "off",
-    "import/no-extraneous-dependencies": "off",
-    "arrow-parens": "off",
-    "global-require": "off",
-    "spaced-comment": "off",
-    "comma-dangle": ["error", {
-      "arrays": "always-multiline",
-      "objects": "always-multiline",
-      "imports": "always-multiline",
-      "exports": "always-multiline",
-      "functions": "ignore",
+  rules: {
+    'react/no-danger': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'arrow-parens': 'off',
+    'global-require': 'off',
+    'spaced-comment': 'off',
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'ignore',
     }]
   },
 
-  "settings": {
-    "import/resolver": {
-      "babel-module": {}
+  settings: {
+    'import/resolver': {
+      'babel-module': {}
     }
   }
 };
