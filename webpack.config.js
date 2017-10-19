8f4fd94e56a423cd4851b7edcef85e4541338350
Vitaly Rtishchev <rtivital@gmail.com>
2017-10-19 19:03:11 +0300

    remove json from extensions resolving

--- webpack.config.js
+++ webpack.config.js
@@ -114,7 +114,7 @@ module.exports = {
 
   resolve: {
     modules: [path.join(__dirname, 'src'), 'node_modules'],
-    extensions: ['.js', '.jsx', '.json'],
+    extensions: ['.js', '.jsx'],
   },
 
   module: { loaders },
