50e4fdda938a6f408ffb4c5b732858f592e48dc4
Vitaly Rtishchev <rtivital@gmail.com>
2018-01-15 17:23:37 +0300

    add new rules exceptions to eslint config and comments about all rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,11 +7,16 @@ module.exports = {
     node: true,
     'jest/globals': true,
   },
+
   rules: {
-    'react/no-danger': 'off',
-    'arrow-parens': 'off',
+    // disabled for condition && someFunc()
+    'no-unused-expressions': 'off',
+
+    // backend developers like _, no need to transform data all the time
+    camelcase: 'off',
+
+    // just the formatting issues, prettier does all the job
     'object-curly-newline': 'off',
-    'global-require': 'off',
     'spaced-comment': 'off',
     'comma-dangle': [
       'error',
@@ -23,6 +28,38 @@ module.exports = {
         functions: 'ignore',
       },
     ],
+
+    // increase max line length to 100
+    'max-len': [
+      'error',
+      100,
+      {
+        ignoreTrailingComments: true,
+        ignoreComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
+
+    // pray that backend developer will send sanatized html
+    'react/no-danger': 'off',
+
+    // there is no need to pass default props all the time
+    'react/require-default-props': 'off',
+
+    // sort react component properties whatever you like
+    'react/sort-comp': 'off',
+
+    // sometimes there is no alternative
+    'react/no-array-index-key': 'off',
+
+    // redux actions do not work well with this
+    'import/prefer-default-export': 'off',
+
+    // this rule throws an error with imported prop-types
+    // https://github.com/yannickcr/eslint-plugin-react/issues/1389
+    'react/no-typos': 'off',
   },
 
   settings: {
