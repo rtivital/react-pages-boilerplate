17eb0388329542b80dee09c94aa620a23d25cdbb
Vitaly Rtishchev <rtivital@gmail.com>
2018-01-15 17:52:10 +0300

    add prettier plugin information to config file

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,6 @@
+// You will probably want to use prettier plugin for your text editor instead of cli version
+// Search for plugin for your text editor here – https://prettier.io/
+
 module.exports = {
   printWidth: 100,
   singleQuote: true,
