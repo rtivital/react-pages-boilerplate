99248867f44a281be67ebfa262e140acb1a9502a
Vitaly Rtishchev <rtivital@gmail.com>
2018-01-31 19:52:03 +0300

    add enzyme-to-json jest serializer

--- enzyme.setup.js
+++ enzyme.setup.js
@@ -1,4 +1,6 @@
-const Enzyme = require('enzyme');
-const EnzymeAdapter = require('enzyme-adapter-react-16');
+/* eslint-disable import/no-extraneous-dependencies */
+
+import Enzyme from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   setupTestFrameworkScriptFile: '<rootDir>/enzyme.setup.js',
   moduleFileExtensions: ['js', 'json', 'jsx'],
   moduleNameMapper: { '\\.(css|scss|sass)$': 'identity-obj-proxy' },
+  snapshotSerializers: ['enzyme-to-json/serializer'],
   transform: {
     '\\.(js|jsx)$': '<rootDir>/node_modules/babel-jest',
     '^(?!.*\\.(css|scss|sass)$)': 'identity-obj-proxy',
