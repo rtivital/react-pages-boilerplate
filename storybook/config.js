3537f770a7dcddd653a7f88bc164a657347c564f
Vitaly Rtishchev <rtivital@gmail.com>
2018-02-01 12:50:35 +0300

    load stories automatically with webpack require context

--- storybook/config.js
+++ storybook/config.js
@@ -1,7 +1,14 @@
 import { configure } from '@storybook/react';
 
+const requireContext = require.context('../src', true, /.story.jsx/);
+
 function loadStories() {
-  require('./stories'); // eslint-disable-line global-require
+  // eslint-disable-next-line global-require
+  require('../src/styles/index');
+  
+  requireContext.keys().forEach((filename) => {
+    requireContext(filename);
+  });
 }
 
 configure(loadStories, module);
