1736d693dd29e04018ff72c35245aca8fb4f72b9
Vitaly Rtishchev <rtivital@gmail.com>
2020-09-09 11:34:23 +0300

    Migration part 1

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,10 @@ module.exports = {
   verbose: true,
   setupTestFrameworkScriptFile: '<rootDir>/enzyme.setup.js',
   moduleFileExtensions: ['js', 'json', 'jsx'],
-  moduleNameMapper: { '\\.(css|scss|sass)$': 'identity-obj-proxy' },
+  moduleNameMapper: { '\\.(css|less)$': 'identity-obj-proxy' },
   snapshotSerializers: ['enzyme-to-json/serializer'],
   transform: {
     '\\.(js|jsx)$': '<rootDir>/node_modules/babel-jest',
-    '^(?!.*\\.(css|scss|sass)$)': 'identity-obj-proxy',
+    '^(?!.*\\.(css|less)$)': 'identity-obj-proxy',
   },
 };
